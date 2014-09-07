1de932ed9ac75d59b86433d253331041010e00af
Oliver Jones <oapjones@gmail.com>
2014-09-07 22:28:56 +0200

    link reviews with restaurants

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140907170815) do
+ActiveRecord::Schema.define(version: 20140907201435) do
 
   create_table "restaurants", force: true do |t|
     t.string   "name"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20140907170815) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
+    t.integer  "restaurant_id"
   end
 
   create_table "users", force: true do |t|
